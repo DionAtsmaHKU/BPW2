4d9315095f18b7a89c116f70fe3c493b18e68f74
DionAtsma <dionatsma@gmail.com>
2024-02-19 16:32:25 +0100

    Tilemap + grid-based movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,22 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
-    // HOi git doin! 
+    public float moveSpeed = 5f;
+    [SerializeField] private Transform movePoint;
+    [SerializeField] private LayerMask whatStopsMovement;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        movePoint.parent = null;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        PlayerMovement();
+    }
+
+    private void PlayerMovement()
+    {
+        transform.position = Vector3.MoveTowards(transform.position,
+                             movePoint.position, moveSpeed * Time.deltaTime);
+
+        Vector3 movement = new Vector3(Input.GetAxisRaw("Horizontal"),
+                                       Input.GetAxisRaw("Vertical"), 0);
+
+        if (Vector3.Distance(transform.position, movePoint.position) <= 0.05f)
+        {
+            if (Mathf.Abs(movement.x) == 1f && !Physics2D.OverlapCircle(movePoint.position
+                                + new Vector3(movement.x, 0, 0), 0.1f, whatStopsMovement))
+            {
+                movePoint.position += new Vector3(movement.x, 0, 0);
+            }
+
+            else if (Mathf.Abs(movement.y) == 1f && !Physics2D.OverlapCircle(movePoint.position
+                                + new Vector3(0, movement.y, 0), 0.1f, whatStopsMovement))
+            {
+                movePoint.position += new Vector3(0, movement.y, 0);
+            }
+        }
     }
 }
